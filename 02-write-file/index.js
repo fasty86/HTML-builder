9f1ce6fd233a9ae777356cc4f84f10c25f34f6cd
fasty86 <oleg.tolm1986@gmail.com>
2023-05-02 14:24:43 +0300

    feat: implement solution for task 02-write-file

---
+++ 02-write-file/index.js
@@ -0,0 +1,21 @@
+const fs = require('fs');
+const path = require('path');
+const process = require('process');
+const readLine = require('readline').promises;
+
+const root = path.resolve(__dirname, 'text.txt');
+const writeStream = fs.createWriteStream(root);
+const rl = readLine.createInterface({ input: process.stdin, output: process.stdout });
+
+console.log('You can type somthing in terminal if you want?');
+
+rl.on('line', data => {
+  if (data === 'exit')  rl.close();
+  else writeStream.write(`${data}\n`);
+})
+process.on('beforeExit', () => {
+  exit();
+});
+function exit() {
+  console.log('Goodbye!');
+}
